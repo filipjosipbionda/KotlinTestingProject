f29822d3837ada5f254fbb4a3fa3efea19e98887
fjbionda <filip.josip.bionda@gmail.com>
2025-01-28 01:27:09 +0100

    Refactor Match.kt code

--- src/main/kotlin/models/Match.kt
+++ src/main/kotlin/models/Match.kt
@@ -20,10 +20,6 @@ class Match(
         team.captureDrake()
     }
 
-    fun captureElderDrake(team: Team) {
-        team.captureElderDrake()
-    }
-
     fun destroyTower(team: Team) {
         if (team == teamBlue && redTowers > 0) {
             redTowers--
@@ -38,23 +34,7 @@ class Match(
         team.addGold(amount)
     }
 
-    fun getTotalTeamGold(teamName: String): Long? {
-        return when (teamName) {
-            teamBlue.name -> teamBlue.totalGold
-            teamRed.name -> teamRed.totalGold
-            else -> null
-        }
-    }
-
     fun incrementMatchTime(minutes: Int) {
         duration += minutes
-    }
-
-    fun checkForWinner() {
-        if (teamBlue.towersDestroyed >= 11 || teamBlue.baronKills >= 3 || teamBlue.elderDrakeKills >= 2) {
-            winner = teamBlue.name
-        } else if (teamRed.towersDestroyed >= 11 || teamRed.baronKills >= 3 || teamRed.elderDrakeKills >= 2) {
-            winner = teamRed.name
-        }
     }
 }
